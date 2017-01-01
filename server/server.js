804b4ca3e3940b783ef036c59e495b72ecba2a96
PiotrBerebecki <petebere@gmail.com>
2017-01-01 13:34:59 +0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -27,6 +27,20 @@ app.post('/todos', (req, res) => {
   
 });
 
+
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    
+    // don't do: res.send(todos) as you will get an array
+    // and you can attach any additional data if you want
+    // instad create an object
+    res.send({todos});
+  }, err => {
+    res.status(400).send(err);
+  });
+});
+
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
