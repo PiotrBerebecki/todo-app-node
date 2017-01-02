5100e6b5af8fba8cd7e3e0f6b29aab74ef4dc940
PiotrBerebecki <petebere@gmail.com>
2017-01-02 09:42:50 +0700

    Setup separate test and development env variables

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ var { User } = require('./models/user');
 
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // thanks to this we can send json to our Express app
 app.use(bodyParser.json());
