6810d94246411bf1fdc49c53409de092eab64ef0
PiotrBerebecki <petebere@gmail.com>
2017-01-04 06:51:16 +0700

    Add hashing playground

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,36 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+
+// Hashing
+var password = '123abc!';
+
+// Generate salt
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$tt03wjCfdMk28CtNZ7GjieyGGQuar/nDQOaS.HLk6AHkrR3syO7rS';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);  // true if hashedPassword matches password
+});
+
+
 
 // JWT using jsonwebtoken package
-var data = {
-  id: 10
-};
+// var data = {
+//   id: 10
+// };
 
-var token = jwt.sign(data, '123abc');
-console.log('token', token);
+// var token = jwt.sign(data, '123abc');
+// console.log('token', token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 
