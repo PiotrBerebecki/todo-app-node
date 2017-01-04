e038e6991636b552bd3de782609e734c51be33b7
PiotrBerebecki <petebere@gmail.com>
2017-01-04 17:11:01 +0700

    Test DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -147,9 +147,9 @@ app.post('/users/login', (req, res) => {
 
 app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
-    req.status(200).send();
+    res.status(200).send();
   }, () => {
-    req.status(200).send();
+    res.status(400).send();
   });
 });
 
