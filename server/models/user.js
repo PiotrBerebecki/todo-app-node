c6756374aeddb1ee798011e64689c4a458fd9294
PiotrBerebecki <petebere@gmail.com>
2017-01-04 15:15:36 +0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,27 @@ UserSchema.statics.findByToken = function (token) {
 };
 
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  
+  return User.findOne({email}).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+    
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+
 // Run code before save event
 // Use regular function if you want to 
 // use 'this' binding
